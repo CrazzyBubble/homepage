0a22627909787ce21cc43ddab26c5d2d35d96323
lex.torbanov <torbanowmain@gmail.com>
2020-05-06 20:44:33 +0300

    Adding change color buttons and fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,24 @@ skills.addEventListener("click", function() {
         player.playVideo();
     }
 });
-
 let butAudio = document.querySelector("#you-must");
 butAudio.addEventListener("click", function() {
     document.querySelector("#you-must audio").play();
 });
+
+function addEventClickChangeColor(button, color) {
+    button.addEventListener("click", function() {
+        page.style.borderColor = color;
+    });
+}
+let page = document.querySelector(".page");
+let butYellow = document.querySelector(".change-color.yellow");
+let butGreen = document.querySelector(".change-color.green");
+let butPurpure = document.querySelector(".change-color.purpure");
+let butIndigo = document.querySelector(".change-color.indigo");
+let butMidnightblue = document.querySelector(".change-color.midnightblue");
+addEventClickChangeColor(butYellow, "#d4b60b");
+addEventClickChangeColor(butGreen, "#0ad125");
+addEventClickChangeColor(butPurpure, "#bb0743");
+addEventClickChangeColor(butIndigo, "indigo");
+addEventClickChangeColor(butMidnightblue, "midnightblue");
